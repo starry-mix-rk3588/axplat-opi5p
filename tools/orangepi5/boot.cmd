9ffaa6302f199616442a8a98b3d6b7d3aa5c73fa
Debin <luodeb@outlook.com>
2025-10-31 16:39:58 +0800

    feat(tools/orangepi5): update network configuration for boot script
    Change IP addresses and add network mask and gateway settings
    to match the new network environment requirements.

--- tools/orangepi5/boot.cmd
+++ tools/orangepi5/boot.cmd
@@ -1,6 +1,8 @@
 bootdev hunt ethernet
-setenv ipaddr 192.168.66.103
-setenv serverip 192.168.66.102
+setenv ipaddr 10.19.0.107
+setenv serverip 10.1.135.123
+setenv netmask 255.255.254.0
+setenv gatewayip 10.19.0.1
 tftp 0x400000 kernel.uimg
 tftp 0x300000 rk3588-orangepi-5-plus.dtb
 bootm 0x400000 - 0x300000
